424d7a00eff1c02f36e223cfcbb13bf483a18565
Utkarsh Nagdev <utkarsh2nagdev@gmail.com>
2021-09-02 19:07:25 +0530

    Q3. Greedy

---
+++ Elements of programming/Greedy Algorithms/scheduleSQLQueries.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+#include<unordered_set>
+using namespace std;
+
+typedef vector <int> vi;
+typedef pair< int ,int > pii;
+#define endl "\n"
+#define sd(val) scanf("%d",&val)
+#define ss(val) scanf("%s",&val)
+#define sl(val) scanf("%lld",&val)
+#define debug(val) printf("check%d\n",val)
+#define all(v) v.begin(),v.end()
+#define PB push_back
+#define MP make_pair
+#define FF first
+#define SS second
+#define ll long long
+#define MOD 1000000007
+#define clr(val) memset(val,0,sizeof(val))
+#define what_is(x) cerr << #x << " is " << x << endl; 
+#define OJ \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define spacing << " " 
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int minimumWaitingTime(vector<int>& query) {
+  sort(query.begin(), query.end());
+  int ans = 0;
+  int n = query.size();
+  for(int i = 0; i < n - 1; i++) {
+    ans += query[i] * (n - (i + 1));
+  } 
+  return ans;
+}
+
+int main() {
+    FIO
+    OJ
+    int size;
+    cin >> size;
+    vector<int> arr(size);  
+    for(auto &it : arr) {
+        cin >> it;
+    }
+    cout << minimumWaitingTime(arr);
+    return 0;
+}
